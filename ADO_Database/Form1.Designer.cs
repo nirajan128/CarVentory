3dd9edb27e1e45b7eb096fbff431d78a199832e8
Nirajan <nirajanshrestha672@gmail.com>
2024-12-08 19:59:12 -0500

    Windows form Created

---
+++ ADO_Database/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace ADO_Database
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusLable = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // statusLable
+            // 
+            this.statusLable.AutoSize = true;
+            this.statusLable.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.statusLable.Location = new System.Drawing.Point(29, 18);
+            this.statusLable.Name = "statusLable";
+            this.statusLable.Size = new System.Drawing.Size(513, 20);
+            this.statusLable.TabIndex = 0;
+            this.statusLable.Text = "Requirement: Current Version of MYSQL on yout local machine";
+            this.statusLable.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.statusLable.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.statusLable);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label statusLable;
+    }
+}
+
